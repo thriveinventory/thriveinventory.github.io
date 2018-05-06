7f04eed9755449af13903836621799b52f9090db
Jennifer Wilson <jenniferwilson@Jennifers-MacBook-Pro.local>
2018-05-06 18:34:28 -0400

    updated so dashboard is first page not main

--- server.js
+++ server.js
@@ -6,21 +6,29 @@ app.listen(3000, function() {
 })
 
 app.get('/', function(request, response) {
-	response.sendFile('/Users/jenniferwilson/Desktop/thrive_inventory' + '/main.html')
+	response.sendFile('/Users/jenniferwilson/Desktop/ThriveCode' + '/dashboard.html')
+})
+
+app.get('/rentalsform.html', function(request, response){
+	response.sendFile('/Users/jenniferwilson/Desktop/ThriveCode' + '/rentalsform.html')
+})
+
+app.get('/main.html', function(request, response){
+	response.sendFile('/Users/jenniferwilson/Desktop/ThriveCode' + '/main.html')
 })
 
 app.get('/indexreusables.html', function(request, response) {
-	response.sendFile('/Users/jenniferwilson/Desktop/thrive_inventory' + '/indexreusables.html')
+	response.sendFile('/Users/jenniferwilson/Desktop/ThriveCode' + '/indexreusables.html')
 })
 
 app.get('/indexgiveaways.html', function(request, response) {
-	response.sendFile('/Users/jenniferwilson/Desktop/thrive_inventory' + '/indexgiveaways.html')
+	response.sendFile('/Users/jenniferwilson/Desktop/ThriveCode' + '/indexgiveaways.html')
 })
 
 app.get('/indexmisc.html', function(request, response) {
-	response.sendFile('/Users/jenniferwilson/Desktop/thrive_inventory' + '/indexmisc.html')
+	response.sendFile('/Users/jenniferwilson/Desktop/ThriveCode' + '/indexmisc.html')
 })
 
 app.get('/indexprog.html', function(request, response) {
-	response.sendFile('/Users/jenniferwilson/Desktop/thrive_inventory' + '/indexprog.html')
+	response.sendFile('/Users/jenniferwilson/Desktop/ThriveCode' + '/indexprog.html')
 })
